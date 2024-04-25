8ae5f1204e1f489c86893ae193d9fd71e6f683db
dipayansh8 <dipayansh8@gmail.com>
2024-04-25 15:44:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+dayNightTheme = () => {
+  let date = new Date();
+  let hour = date.getHours();
+  console.log(hour);
+
+  if (hour >= 7 && hour < 19) {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+  else {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+  }
+}
+
+window.addEventListener('load', dayNightTheme);
+
+document.querySelector("#input").addEventListener("keydown", (event) => {
+  if (event.key == "Enter")
+    apiRequest();
+});
+
+document.querySelector("#search").addEventListener("click", () => {
+  apiRequest();
+});
+
+apiRequest = async () => {
+  try {
+
+    document.querySelector("#grid").textContent = "";
+
+    const response = await fetch('https://api.unsplash.com/search/photos?query=' + input.value + '&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo');
+
+    const data = await response.json();
+    console.log(data);
+    loadImages(data);
+  }
+
+  catch (error) {
+    console.log(error);
+  }
+
+  loadImages = (data) => {
+    for (let i = 0; i < data.results.length; i++) {
+      let image = document.createElement("div");
+      image.className = "img";
+      image.style.backgroundImage = "url(" + data.results[i].urls.raw + "&w=1366&h=768" + ")";
+      image.addEventListener("dblclick", function () {
+        window.open(data.results[i].links.download, '_blank');
+      })
+      document.querySelector("#grid").appendChild(image);
+    }
+  }
+}
